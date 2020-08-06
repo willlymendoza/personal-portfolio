65514745cf03c106164cbf88d08cfac179f36f6c
Willian <wyllmh5@gmail.com>
2020-08-06 16:18:41 -0600

    Added fonts and reseters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.scss";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 function App() {
   return (
-    <div>
-      <h1>jj</h1>
-    </div>
+    <Fragment>
+      <Link to="third" smooth={true} duration={750}>
+        Move to second
+      </Link>
+
+      <div>
+        <div className="scroll-item" id="first">
+          FIRST SCROLL
+        </div>
+        <div className="scroll-item" id="second">
+          SECOND SCROLL
+        </div>
+        <div className="scroll-item" id="third">
+          THIRD SCROLL
+        </div>
+
+        <div onClick={() => scroll.scrollToTop()}>Scroll to top</div>
+      </div>
+    </Fragment>
   );
 }
 
