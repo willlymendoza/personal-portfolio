153283430167125368a3dd6d8000386e264d9499
Willian <wyllmh5@gmail.com>
2020-08-06 14:33:23 -0600

    Removed fullpage js package

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactFullpage from "@fullpage/react-fullpage";
 import App from "./App";
 
-const Fullpage = () => (
-  <ReactFullpage
-    //fullpage options
-    licenseKey={"YOUR_KEY_HERE"}
-    scrollingSpeed={1000} /* Options here */
-    anchors={["about", "portfolio", "resume", "contact"]}
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveTo(2)}>
-              Click me to move down
-            </button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Fullpage />, document.getElementById("root"));
