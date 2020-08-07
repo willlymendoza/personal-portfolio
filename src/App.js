b6fe7e27cd6237f28d0d1870857adf01c4dbf65c
Willian <wyllmh5@gmail.com>
2020-08-06 20:27:23 -0600

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Link, animateScroll as scroll } from "react-scroll";
 import Container from "components/Container";
 import Header from "components/Header";
 import Main from "components/Main";
+import Footer from "components/Footer";
 
 function App() {
   return (
     <Container>
       <Header />
       <Main />
+      <Footer />
     </Container>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles.scss";
+
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <div class="footer-content">
+        <div class="footer-content-name">
+          <h2>Willian Mendoza</h2>
+          <h3>Software Engineer</h3>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
