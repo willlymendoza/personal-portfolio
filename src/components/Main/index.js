cf0c11aa427f58fc90330400d8ecd475c74ad7d4
Willian <wyllmh5@gmail.com>
2020-08-06 19:24:12 -0600

    Added Main component

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.scss";
+import Portfolio from "components/Portfolio";
+
+const index = () => {
+  return (
+    <main className="main">
+      <div className="main-content">
+        <Portfolio />
+      </div>
+    </main>
+  );
+};
+
+export default index;
