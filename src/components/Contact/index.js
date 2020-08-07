804f3e7cd81222772a55f58cd54869b3ece6a55e
Willian <wyllmh5@gmail.com>
2020-08-06 20:34:34 -0600

    Fixed classes names

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,19 +3,19 @@ import "./styles.scss";
 
 const Contact = () => {
   return (
-    <div id="contact" class="contact">
-      <h1 class="title">Contact</h1>
+    <div id="contact" className="contact">
+      <h1 className="title">Contact</h1>
       <h2>Let's get in touch</h2>
       <h3>
         You can send me an email or a direct message to my linkedin profile
       </h3>
 
-      <div class="contact-socials">
+      <div className="contact-socials">
         <a href="#">
-          <i class="fa fa-envelope"></i>
+          <i className="fa fa-envelope"></i>
         </a>
         <a href="#">
-          <i class="fab fa-linkedin"></i>
+          <i className="fab fa-linkedin"></i>
         </a>
       </div>
     </div>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import "./styles.scss";
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="footer-content">
-        <div class="footer-content-name">
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-content-name">
           <h2>Willian Mendoza</h2>
           <h3>Software Engineer</h3>
         </div>
