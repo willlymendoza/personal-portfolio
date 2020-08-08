297accd00e1b06c2e7eb7fcfd8756f30fd84cf31
Willian <wyllmh5@gmail.com>
2020-08-07 19:52:30 -0600

    Added links to resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,10 +6,18 @@ const Resume = () => {
     <div id="resume" className="resume">
       <h1 className="title">Resume</h1>
       <div className="resume-links">
-        <a href="#">
+        <a
+          href="https://tpss.s3.amazonaws.com/resume_en.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Resume EN <i className="fa fa-download"></i>
         </a>
-        <a href="#">
+        <a
+          href="https://tpss.s3.amazonaws.com/resume_es.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Resume ES <i className="fa fa-download"></i>
         </a>
       </div>
